3b06721378e72dc624aab501feafe4d8fb2a82aa
Sergiu <chilatsergiu@gmail.com>
2021-09-20 10:21:27 +0300

    Initial commit

---
+++ src/main/java/com/springboot_sample/springboot_sample/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.springboot_sample.springboot_sample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String mainPage(){
+        return "This is main page";
+    }
+}
---
+++ src/test/java/com/springboot_sample/springboot_sample/SpringbootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_sample.springboot_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
